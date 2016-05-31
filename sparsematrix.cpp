a01f3deda7055dff962a5f59f72a8e11cce32994
Adam M <lukwap@gmail.com>
2016-05-31 17:01:31 +0200

    [Untested] SparseMatrix

--- sparsematrix.cpp
+++ sparsematrix.cpp
@@ -7,22 +7,140 @@
 #include <cstdlib>
 #include <cassert>
 
+#define ROW 0
+#define COL 1
+#define VALUE 2
+
 
 SparseMatrix* SparseMatrix::createFromFile(const char* filepath)
 {
 	FILE *f = fopen(filepath, "r");
-	if (!f) {
+	if (!f) 
+	{
 		printf("Error opening file\n");
 		return nullptr;
 	}
 
-	return nullptr;
+	uint rows, cols, elems, maxElems;
+
+	fscanf(f, "%u%u%u%u", &rows, &cols, &elems, &maxElems);
+
+	std::vector<double> a(elems);
+	std::vector<uint>  ia(rows + 1);
+	std::vector<uint> ja(elems);
+
+	for (auto &aElem : a) 
+	{
+		fscanf(f, "%lf", &aElem);
+	}
 
 
+	for (auto &iaElem : ia) 
+	{
+		fscanf(f, "%u", &iaElem);
+	}
+
+	for (auto &jaElem : ja) 
+	{
+		fscanf(f, "%u", &jaElem);
+	}
+
+	fclose(f);
+
+	//now we create matrix
+	data = new SparseMatrix::dataType[a.size()];
+
+	uint row, amountInRow, col;
+
+	col = 1; // we start from 1 columns of matrix
+
+	for (size_t  i = 0; i < a.size(); i++)
+	{
+		if(amountInRow >= ia[col])
+		{
+			col++;
+			amountInRow = 0;
+			row++;
+		}
+
+		data[i] = std::make_tuple(ja[i], row, a[i]);
+	}
+
+	numValues = a.size();
+
+	return this;
 }
 
+double SparseMatrix::findElem(const uint row, const uint col)
+{
+	double result = 0.0;
+	for(size_t index = 0; index < numValues; index++)
+	{
+		if (std::get<ROW>(data[index]) == row && std::get<COL>(data[index]) == col)
+		{
+			result = std::get<VALUE>(data[index]);
+			index = numValues;
+		}
+	}
+	return result;
+}
+
+std::vector<SparseMatrix::dataType> SparseMatrix::findElemsFixCol(const uint col)
+{
+	return this->findValueFixColOrRow(false, col);
+}
+
+std::vector<SparseMatrix::dataType> SparseMatrix::findElemsFixRow(const uint row)
+{
+	return this->findValueFixColOrRow(true, row);
+}
+
+SparseMatrix SparseMatrix::extractSparseMatrix(const uint colBegin, const uint colEnd, const uint rowBegin, const uint rowEnd)
+{
+	std::vector<SparseMatrix::dataType> valueHolder;
+	
+	for(size_t index = 0; index < numValues; index++)
+	{
+		//check ROW
+		if(std::get<ROW>(data[index]) >= rowBegin && std::get<ROW>(data[index]) <= rowEnd)
+		{
+			//check COL
+			if(std::get<COL>(data[index]) >= colBegin && std::get<COL>(data[index]) <= colEnd)
+			{
+				valueHolder.push_back(data[index]);
+			}
+		}
+	}
+	SparseMatrix* result = new SparseMatrix();
+
+	//FIXME pointer issues maybe? maybe i should copy it
+	result->data = valueHolder.data();
+	result->numValues = valueHolder.size();
+
+	return *result;
+}
+
+
+std::vector<SparseMatrix::dataType> SparseMatrix::findValueFixColOrRow(const bool fixedRow, const uint cord)
+{
+	std::vector<SparseMatrix::dataType> result;
+	uint dummy;
+
+	for(size_t index = 0; index < numValues; index++)
+	{
+		//here we decide whether it should be row or col to be checked
+		dummy = fixedRow ?  std::get<ROW>(data[index]) : std::get<COL>(data[index]);
+
+		if (dummy == cord)
+		{
+			result.push_back(data[index]);
+		}
+	}
+	return result;
+}
 
 SparseMatrix::~SparseMatrix() 
 {
-	
+	delete [] data;
 }
+
--- sparsematrix.hpp
+++ sparsematrix.hpp
@@ -12,24 +12,34 @@ class SparseMatrix {
 public:
 	SparseMatrix(){}
 	~SparseMatrix();
+	typedef std::tuple<uint, uint, double> dataType;
+
+
 	
 	SparseMatrix* createFromFile(const char* filepath);
 	// SparseMatrix slice() const;
 	// double elem(const uint x, const uint y);
 	// const uint getDimm() const;
 	// MPI_Datatype getMPI_Type() const;
-	
+
+	double findElem(const uint row, const uint col);
+	std::vector<SparseMatrix::dataType> findElemsFixRow(const uint row);
+	std::vector<SparseMatrix::dataType> findElemsFixCol(const uint col);
+	SparseMatrix extractSparseMatrix(const uint colBegin, const uint colEnd, const uint rowStart, const uint rowEnd);
+
 private:
-	typedef std::tuple<uint, uint, double> data_type;
+	//typedef std::tuple<uint, uint, double> dataType;
 	// SparseMatrix(const SparseMatrix &other);
 	// SparseMatrix& operator=(const SparseMatrix &other) = delete;
 	// SparseMatrix(SparseMatrix &&other);
 	// SparseMatrix& operator=(SparseMatrix &&other) = delete;
 	
 	void seek(const uint x, const uint y);
+
+	std::vector<SparseMatrix::dataType> findValueFixColOrRow(const bool fixedRow, const uint cord);
 	
-	std::tuple<uint, uint, double> *values;	
-	uint num_values;
+	std::tuple<uint, uint, double> *data;	
+	uint numValues;
 	
 };
 
